09954e139b0df8cd449e922846f7512e3d019411
reeshaiqbal <reeshaiqbal62@gmail.com>
2026-01-03 15:14:19 +0500

    Modified the list using dictionary and added __main__ as program's entry point

--- Todo.py
+++ Todo.py
@@ -4,7 +4,8 @@ class TodoList:  # I created a class and enclosed "data" and "methods" in it imp
 
     def add_task(self):
         add = input("Write your task here: ")
-        self.tasks.append([add, False])
+        # Used dictionary for better readability of tasks
+        self.tasks.append({"title": add, "done": False})
         print("Task Added Successfully!")
 
     def view_task(self):
@@ -12,16 +13,16 @@ class TodoList:  # I created a class and enclosed "data" and "methods" in it imp
             print("No Task available yet")
         else:
             for i in range(len(self.tasks)):
-                if self.tasks[i][1] == True:
+                if self.tasks[i]["done"] == True:
                     status = "Done"
                 else:
                     status = "Pending"
-                print(i+1, self.tasks[i][0], status)
+                print(i+1, self.tasks[i]["title"], status)
 
     def mark_as_done(self):
         number = int(input("Enter the Task number: "))
         if 1 <= number <= len(self.tasks):
-            self.tasks[number - 1][1] = True
+            self.tasks[number - 1]["done"] = True
             print("Task marked as done")
         else:
             print("Invalid task number")
@@ -38,12 +39,12 @@ class TodoList:  # I created a class and enclosed "data" and "methods" in it imp
         word = input("Enter the word you want to search: ")
         found = False
         for i in range(len(self.tasks)):
-            if word.lower() in self.tasks[i][0].lower():
-                if self.tasks[i][1] == True:
+            if word.lower() in self.tasks[i]["title"].lower():
+                if self.tasks[i]["done"] == True:
                     status = "Done"
                 else:
                     status = "Pending"
-                print(i+1, self.tasks[i][0], status)
+                print(i+1, self.tasks[i]["title"], status)
                 found = True
         if found == False:
             print("Desired Task not found")
@@ -52,44 +53,45 @@ class TodoList:  # I created a class and enclosed "data" and "methods" in it imp
         number = int(input("Enter the task you want to update: "))
         if 1 <= number <= len(self.tasks):
             new_text = input("Enter the new text here: ")
-            self.tasks[number-1][0] = new_text
+            self.tasks[number-1]["title"] = new_text
             print("Task Updated Successfully!")
 
 
-todo = TodoList()  # Object of the class is created
+if __name__ == "__main__":
+    todo = TodoList()  # Object of the class is created
 
-while True:  # Loop will be repeated and menu will be shown again and again until the loop breaks
-    print("\n1. Add Task")
-    print("2. View Tasks")
-    print("3. Mark Task as Done")
-    print("4. Delete Task")
-    print("5. Search Task")
-    print("6. Update Task")
-    print("7. Exit")
+    while True:  # Loop will be repeated and menu will be shown again and again until the loop breaks
+        print("\n1. Add Task")
+        print("2. View Tasks")
+        print("3. Mark Task as Done")
+        print("4. Delete Task")
+        print("5. Search Task")
+        print("6. Update Task")
+        print("7. Exit")
 
-    choice = input("Choose: ")
+        choice = input("Choose: ")
 
-    if choice == "1":
-        todo.add_task()
+        if choice == "1":
+            todo.add_task()
 
-    elif choice == "2":
-        todo.view_task()
+        elif choice == "2":
+            todo.view_task()
 
-    elif choice == "3":
-        todo.mark_as_done()
+        elif choice == "3":
+            todo.mark_as_done()
 
-    elif choice == "4":
-        todo.delete_task()
+        elif choice == "4":
+            todo.delete_task()
 
-    elif choice == "5":
-        todo.search_task()
+        elif choice == "5":
+            todo.search_task()
 
-    elif choice == "6":
-        todo.update_task()
+        elif choice == "6":
+            todo.update_task()
 
-    elif choice == "7":
-        print("Program Ended")
-        break
+        elif choice == "7":
+            print("Program Ended")
+            break
 
-    else:
-        print("Invalid choice")
+        else:
+            print("Invalid choice")
